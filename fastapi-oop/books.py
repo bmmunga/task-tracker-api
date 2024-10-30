0ac1869d5d92f49f4809e4a9d9538736b4102ea7
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-10-30 11:14:39 +0000

    feat: add field validation

--- fastapi-oop/books.py
+++ fastapi-oop/books.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -21,17 +22,18 @@ class Book:
 
 # Request model for use in post request
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description:str
-    rating: int
+    # None required for pydantiv v2
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description:str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
 
 
 BOOKS = [
-    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book', 5),
-    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'This is a great book', 5),
-    Book(3, 'Master Endpoints', 'codingwithroby', 'This is an awesome book', 5),
+    Book(1, 'Computer Science Pro', 'thefreelancer', 'A very nice book', 5),
+    Book(2, 'Be Fast with FastAPI', 'thefreelancer', 'This is a great book', 5),
+    Book(3, 'Master Endpoints', 'the freelancer', 'This is an awesome book', 5),
     Book(4, 'HP1', 'Author 1', 'Book Description', 2),
     Book(5, 'HP2', 'Author 2', 'Book Description', 3),
     Book(6, 'HP3', 'Author 3', 'Book Description', 1),
@@ -47,5 +49,11 @@ async def read_all_books():
 # So we use pydantic in python
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.dict()) #.model_dump() new fastapi
-    print(type(new_book))
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
+
+def find_book_id(book: Book):
+    if book.id > 0:
+        book.id = BOOKS[-1].id + 1
+    else:
+        book.id = 1
+    return book
