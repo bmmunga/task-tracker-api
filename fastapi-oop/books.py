e1417e3d39237d4b371d8c7b342ecbb2aa600af2
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-10-30 15:29:45 +0000

    feat: add published_date field and function with query param

--- fastapi-oop/books.py
+++ fastapi-oop/books.py
@@ -11,13 +11,16 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 # Request model for use in post request
@@ -28,6 +31,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description:str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
 
     # Model config to set up more descriptive request within swagger docs
     model_config = {
@@ -36,19 +40,20 @@ class BookRequest(BaseModel):
                 "title": "A new book",
                 "author": "thefreelancer",
                 "description": "A new description of a book",
-                "rating": 5
+                "rating": 5,
+                "published_date": 2024
             }
         }
     }
 
 
 BOOKS = [
-    Book(1, 'Computer Science Pro', 'thefreelancer', 'A very nice book', 5),
-    Book(2, 'Be Fast with FastAPI', 'thefreelancer', 'This is a great book', 5),
-    Book(3, 'Master Endpoints', 'the freelancer', 'This is an awesome book', 5),
-    Book(4, 'HP1', 'Author 1', 'Book Description', 2),
-    Book(5, 'HP2', 'Author 2', 'Book Description', 3),
-    Book(6, 'HP3', 'Author 3', 'Book Description', 1),
+    Book(1, 'Computer Science Pro', 'thefreelancer', 'A very nice book', 5, 2023),
+    Book(2, 'Be Fast with FastAPI', 'thefreelancer', 'This is a great book', 5, 2031),
+    Book(3, 'Master Endpoints', 'the freelancer', 'This is an awesome book', 5, 2022),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2, 2020),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3, 2024),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1, 2020),
 ]
 
 
@@ -69,6 +74,15 @@ async def read_book_by_rating(book_rating: int):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/publish/")
+async def read_books_by_publish_date(published_date: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
             books_to_return.append(book)
     return books_to_return
 
