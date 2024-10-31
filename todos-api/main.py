00ea698579997ce1cbf949565d5b0aeb2b0bb71b
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-10-31 22:33:52 +0000

    feat: add post request endpoint

--- todos-api/main.py
+++ todos-api/main.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, Path
 import models
@@ -21,6 +22,14 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
@@ -32,3 +41,11 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail='Todo Not Found')
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todos_model = Todos(**todo_request.dict())
+
+    db.add(todos_model)
+    db.commit()
