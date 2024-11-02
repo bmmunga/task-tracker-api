0fe7a63db88c56c9050bce92475294ade83ba210
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-11-02 19:58:03 +0000

    refactor: add authentication to the todo post endpoint

--- todos-api/routers/todos.py
+++ todos-api/routers/todos.py
@@ -5,6 +5,8 @@ from fastapi import APIRouter, Depends, HTTPException, Path
 from starlette import status
 from models import Todos
 from database import SessionLocal
+from .auth import get_current_user
+
 
 # Similar to Blueprint in Flask for modularizing routes
 router = APIRouter()
@@ -19,6 +21,7 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 class TodoRequest(BaseModel):
@@ -42,8 +45,11 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo_model = Todos(**todo_request.dict())
+async def create_todo(user: user_dependency, db: db_dependency,
+                        todo_request: TodoRequest):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    todo_model = Todos(**todo_request.dict(), owner_id=user.get('id'))
 
     db.add(todo_model)
     db.commit()
