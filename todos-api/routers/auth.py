81dbe6188c1c07ecfa44f1e0093c542953dbc786
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-11-02 09:48:31 +0000

    refactor: add /auth prefix to authentication routes

--- todos-api/routers/auth.py
+++ todos-api/routers/auth.py
@@ -11,13 +11,16 @@ from database import SessionLocal
 from models import Users
 
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
 
 SECRET_KEY = '2d548e101580e851e2c21c1828ca8ddd99385267c743cb30e4bfd787db8ceb8a'
 ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
 class CreateUserRequest(BaseModel):
@@ -73,10 +76,11 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         return {'username': username, 'id': user_id}
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='Could not validate user')
+                                detail='Could not validate user')
 
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+# Prefix in place so /auth removed
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     # Does not use **create_user_request.dict(). Diff in pass & hashed password
     create_user_model = Users(
@@ -98,6 +102,7 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
                                 db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        return 'Failed Authentication'
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user')
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
     return {'access_token': token, 'token_type': 'bearer'}
