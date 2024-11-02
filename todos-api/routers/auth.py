f4532c215e181a647d38987402678c597826e53c
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-11-02 09:31:48 +0000

    feat: decode JSON web token

--- todos-api/routers/auth.py
+++ todos-api/routers/auth.py
@@ -1,7 +1,7 @@
 from datetime import timedelta, datetime, timezone
-from fastapi import APIRouter, Depends
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 from pydantic import BaseModel
 from starlette import status
@@ -17,6 +17,7 @@ SECRET_KEY = '2d548e101580e851e2c21c1828ca8ddd99385267c743cb30e4bfd787db8ceb8a'
 ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
 
 
 class CreateUserRequest(BaseModel):
@@ -59,6 +60,20 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     expires = datetime.now(timezone.utc) + expires_delta
     encode.update({'exp': expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user')
+        return {'username': username, 'id': user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user')
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
