0e609bf6d8effa943cdaa4fb3843ed18ce1a63a3
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-11-03 15:32:15 +0000

    test: initial test cases

---
+++ todos-api/test/test_example.py
@@ -0,0 +1,43 @@
+import pytest
+
+
+def test_is_intance():
+    assert isinstance('this is a string', str)
+    assert not isinstance('20', int)
+
+def test_boolean():
+    validated = True
+    assert validated is True
+    assert ('hello' == 'world') is False
+
+def test_type():
+    assert type('Hello' is str)
+    assert type('World' is not int)
+
+def test_greater_and_less_than():
+    assert 7 > 3
+    assert 4 < 10
+
+def test_list():
+    num_list = [1, 2, 3, 4, 5]
+    any_list = [False, False]
+    assert 1 in num_list
+    assert 7 not in num_list
+    assert all(num_list)
+    assert not any(any_list)
+
+
+class Student:
+    def __init__(self, first_name: str, last_name: str, major: str, years: int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.major = major
+        self.years = years
+
+
+def test_person_initialization():
+    p = Student('John', 'Doe', 'Computer Science', 3)
+    assert p.first_name == 'John'
+    assert p.last_name == 'Doe'
+    assert p.major == 'Computer Science'
+    assert p.years == 3
