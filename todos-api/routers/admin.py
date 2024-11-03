c66ef63c93f780a83a9b505e124be5a5dbc43ba2
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-11-03 11:29:01 +0000

    feat: add admin api endpoints

--- todos-api/routers/admin.py
+++ todos-api/routers/admin.py
@@ -30,7 +30,7 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 async def read_all(user: user_dependency, db: db_dependency):
     if user is None or user.get('user_role') != 'admin':
         raise HTTPException(status_code='404', detail='Authentication Failed')
-    return db.query(Todos).all()
+    return db.query(Todos).all() 
 
 
 @router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
