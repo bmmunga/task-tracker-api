c8ee881a08f7016bd1b4940073badfedd70f83a8
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-11-03 23:20:57 +0000

    test: delete api endpoints

--- todos-api/test/test_admin.py
+++ todos-api/test/test_admin.py
@@ -1,5 +1,7 @@
 from .utils import *
 from ..routers.admin import get_db, get_current_user
+from fastapi import status
+from ..models import Todos
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
@@ -10,3 +12,18 @@ def test_admin_read_all_authenticated(test_todo):
     assert response.json() == [{'complete': False, 'title': 'Learn coding!',
                                 'description': 'Need to practice everyday!',
                                 'id': 1, 'priority': 5, 'owner_id': 1}]
+
+
+def test_admin_delete_todo(test_todo):
+    response = client.delete("/admin/todo/1")
+    assert response.status_code == 204
+
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model is None
+
+
+def test_admin_delete_todo_not_found():
+    response = client.delete("/admin/todo/999")
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Todo Not Found'}
