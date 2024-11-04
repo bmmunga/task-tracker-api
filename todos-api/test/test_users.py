4831d3be24bd415db8cc68918f97ce37e8ed5043
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-11-04 00:06:50 +0000

    test: test password and phone number

--- todos-api/test/test_users.py
+++ todos-api/test/test_users.py
@@ -14,3 +14,21 @@ def test_return_user(test_user):
     assert response.json()['last_name'] == 'Hus'
     assert response.json()['role'] == 'admin'
     assert response.json()['phone_number'] == '11111'
+
+
+def test_change_password_success(test_user):
+    response = client.put("/user/password", json={"password": "test12345",
+                                                    "new_password": "newtest12345"})
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+def test_change_password_invalid_current_password(test_user):
+    response = client.put("/user/password", json={"password": "wrongpassword",
+                            "new_password": "new+password"})
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {'detail': 'Error on password change'}
+
+
+def test_change_phone_number_success(test_user):
+    response = client.put("/user/phonenumber/111111")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
