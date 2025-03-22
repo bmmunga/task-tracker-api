05f13854d29ed125b59911ed234bea5e5114f52c
Boniface Munga <Mwachilumobm@gmail.com>
2025-03-22 10:04:09 +0000

    chore(config): add dotenv support for environment variables

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import os
 from todos_api.models import Base
 from todos_api.database import engine
 from todos_api.routers import auth, todos, admin, users
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -17,3 +22,11 @@ app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
 app.include_router(users.router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    
+    port = int(os.getenv("PORT", 10000))
+    
+    uvicorn.run(app, host="0.0.0.0", port=port)
